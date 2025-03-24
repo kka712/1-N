b1a58b4f3ff5ba1b825fa9e7ce2b9eb6e46cc197
changhee <oche.edu@gmail.com>
2025-03-24 14:35:17 +0900

    Comment Create 기능 구현

--- articles/forms.py
+++ articles/forms.py
@@ -1,8 +1,20 @@
 from django.forms import ModelForm
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(ModelForm):
     class Meta():
         model = Article
         fields = '__all__'
+
+
+class CommentForm(ModelForm):
+    class Meta():
+        model = Comment
+        # fields = '__all__'
+
+        # fields => 추가할 필드 목록
+        # fields = ('content', )
+
+        # exclude => 제외할 필드 목록
+        exclude = ('article', )
         
